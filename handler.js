3c1e3883138d637565111b181f9b9744c7ddfade
punnyapradeep11 <49022622+punnyapradeep11@users.noreply.github.com>
2019-04-11 13:01:39 +0530

    Add files via upload

---
+++ handler.js
@@ -0,0 +1,16 @@
+'use strict';
+var moment = require('moment');
+var custom = require('/opt/nodejs/custom');
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: moment.now(),
+      msg: custom.getHello(),
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
